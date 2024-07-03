e1bfed49b68bdfddd85c4981c58b8493a7d5e76d
IvanOrsh <ksironov@mail.ru>
2024-07-04 00:11:29 +0300

    refactor: extract nav links from nav

--- src/routes/dashboard/Nav.svelte
+++ src/routes/dashboard/Nav.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { Globe, Home, Files, Users2, Power } from 'lucide-svelte';
+	import { Globe } from 'lucide-svelte';
+
+	import NavLinks from './NavLinks.svelte';
 </script>
 
 <nav class="flex h-full flex-col px-3 py-4 md:px-2">
@@ -13,40 +15,6 @@
 			</div>
 		</div></a
 	>
-	<div class="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-		<!-- TODO: hover -->
-		<a
-			aria-label="Home"
-			class="rounded-mdp-3 flex h-[48px] grow items-center justify-center gap-2 bg-sky-100 text-sm font-medium no-underline hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
-			href="/dashboard"
-		>
-			<Home />
-			<p class="hidden md:block">Home</p></a
-		><a
-			aria-label="Invoices"
-			class="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium no-underline hover:bg-sky-100 hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
-			href="/dashboard/invoices"
-		>
-			<Files />
-			<p class="hidden md:block">Invoices</p></a
-		><a
-			aria-label="Customers"
-			class="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium no-underline hover:bg-sky-100 hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
-			href="/dashboard/customers"
-		>
-			<Users2 />
-			<p class="hidden md:block">Customers</p></a
-		>
-		<div class="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
 
-		<!-- Sign Out #TODO -->
-		<form action="" method="post">
-			<button
-				class="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
-			>
-				<Power />
-				<div class="hidden md:block">Sign Out</div></button
-			>
-		</form>
-	</div>
+	<NavLinks />
 </nav>
---
+++ src/routes/dashboard/NavLinks.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { Home, Files, Users2, Power } from 'lucide-svelte';
+	import { page } from '$app/stores';
+
+	const links = [
+		{
+			label: 'Home',
+			href: '/dashboard',
+			icon: Home,
+		},
+
+		{
+			label: 'Invoices',
+			href: '/dashboard/invoices',
+			icon: Files,
+		},
+
+		{
+			label: 'Customers',
+			href: '/dashboard/customers',
+			icon: Users2,
+		},
+	];
+</script>
+
+<div class="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+	<!-- Links -->
+	{#each links as { label, href, icon }}
+		<a
+			aria-label={label}
+			class="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium text-black no-underline hover:bg-sky-100 hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
+			class:activeClasses={$page.url.pathname == href}
+			{href}
+		>
+			<svelte:component this={icon} />
+			<p class="hidden md:block">{label}</p></a
+		>
+	{/each}
+
+	<div class="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+
+	<!-- Sign Out #TODO -->
+	<form action="" method="post">
+		<button
+			class="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-primary-600 md:flex-none md:justify-start md:p-2 md:px-3"
+		>
+			<Power />
+			<div class="hidden md:block">Sign Out</div></button
+		>
+	</form>
+</div>
+
+<style>
+	.activeClasses {
+		@apply bg-sky-100 text-primary-600;
+	}
+</style>
