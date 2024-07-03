6e7d978da389d015768d79869339ea0c60e08e8a
IvanOrsh <ksironov@mail.ru>
2024-07-03 15:21:52 +0300

    feat: scaffold landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="bg-primary-500 mb-4 flex h-20 shrink-0 items-end rounded-lg p-4 md:h-52" />
+
+<main class="flex grow flex-col gap-4 md:flex-row">
+	<div
+		class="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20"
+	>
+		<div
+			class="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+		/>
+		<p class="font-serif text-xl text-gray-800 md:text-3xl md:leading-normal">
+			<strong>Welcome to Acme.</strong> This is the example for the{' '}
+			<a href="https://nextjs.org/learn/"> Next.js Learn Course </a>
+			, brought to you by Vercel.
+		</p>
+
+		<a
+			class="bg-primary-500 hover:bg-primary-400 flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium text-white no-underline transition-colors md:text-base"
+			href="/login"
+			><span>Log in</span>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				aria-hidden="true"
+				data-slot="icon"
+				class="w-5 md:w-6"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+				></path></svg
+			>
+		</a>
+	</div>
+</main>
