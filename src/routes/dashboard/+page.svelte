8257797a1a50052dd5405583b10c2868bbe7bc34
IvanOrsh <ksironov@mail.ru>
2024-07-04 22:05:53 +0300

    feat: implement dashboard/home page - wip

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,19 @@
-<p>Dashboard page</p>
+<script lang="ts">
+	import DashboardCards from './DashboardCards/DashboardCards.svelte';
+	import LatestInvoices from './LatestInvoices/LatestInvoices.svelte';
+	import RevenueChart from './RevenueChart/RevenueChart.svelte';
+
+	export let data;
+</script>
+
+<main>
+	<h1 class="mb-4 font-serif text-xl md:text-2xl">Dashboard</h1>
+	<div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+		<DashboardCards cardData={data.cardData} />
+	</div>
+
+	<div class="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+		<RevenueChart totalRevenue={data.allRevenue} />
+		<LatestInvoices latestInvoices={data.latestInvoices} />
+	</div>
+</main>
