f3e909a4c3959c00a4bb4e95e84fc03641e40214
IvanOrsh <ksironov@mail.ru>
2024-07-07 22:55:18 +0300

    fix: update to deal with strings

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,3 +1,7 @@
-const formatter = new Intl.DateTimeFormat('en-GB', {});
+const formatter = new Intl.DateTimeFormat('en-GB', {
+	dateStyle: 'medium',
+});
 
 export const formatDate = (date: Date) => formatter.format(date);
+
+export const formatIsoDate = (date: string) => formatter.format(new Date(date));
